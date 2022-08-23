9d952934de1718f7b2bdef3fe866ef9540e904d6
codedamage <codedamage@protonmail.com>
2022-08-23 22:14:54 +0300

    Added first test

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Unit;
 
+use App\Models\User;
 use PHPUnit\Framework\TestCase;
 
 class ExampleTest extends TestCase
@@ -14,5 +15,12 @@ class ExampleTest extends TestCase
     public function test_that_true_is_true()
     {
         $this->assertTrue(true);
+    }
+    public function test_api_avalability()
+    {
+        $user = User::factory()->create();
+        $response = $this->actingAs($user)
+            ->get('/api/snippets');
+        $response->assertStatus(200);
     }
 }
